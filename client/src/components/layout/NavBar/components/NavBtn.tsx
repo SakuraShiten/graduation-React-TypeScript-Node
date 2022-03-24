e485634ef8a3fec3d026fbbb54b556b41e3a45cb
Sakura Shiten <a.slyusar.a@gmail.com>
2022-03-25 01:18:23 +0500

    main

--- client/src/components/layout/NavBar/components/NavBtn.tsx
+++ client/src/components/layout/NavBar/components/NavBtn.tsx
@@ -2,10 +2,28 @@ import React, { FC } from 'react'
 import styled from 'styled-components';
 
 const StyledNavBtn = styled.p`
-font-size: 16px;
-color: #000;
-cursor: pointer;
-margin-right: 30px;
+    font-size: 16px;
+    color: #000;
+    cursor: pointer;
+    margin-right: 30px;
+    position: relative;
+    transition: 0.3s;
+    &::before{
+        position:absolute;
+        bottom:0;
+        left:50%;
+        width:0%;
+        transform: translateX(-50%);
+        border-bottom:1.5px solid #000;
+        transition: 0.3s;
+        content:"";
+    }
+    &:hover{
+        transform: scale(105%);
+        &::before{
+            width:100%;
+        }
+    }
 `
 
 const NavBtn: FC = ({ children }) => {
--- client/src/pages/index/Index.tsx
+++ client/src/pages/index/Index.tsx
@@ -1,7 +1,29 @@
 import React, { FC } from 'react'
 import NavBar from '../../components/layout/NavBar/NavBar'
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import fonImage from './static/fon.jpg'
+
+const AnimationBtnFon = keyframes`
+0%{
+    width:100%;
+    bottom:0;
+}
+45%{
+    width:0%;
+    bottom:0;
+    transform: translateX(100%);
+}
+55%{
+    transform: translateX(150%);
+    width:0%;
+    bottom:100%;
+}
+100%{
+    transform: translateX(0%);
+    width:100%;
+    bottom:100%;
+}
+`
 
 const StyledIndexFon = styled.div`
     width: 100vw;
@@ -14,7 +36,6 @@ const StyledIndexFon = styled.div`
     flex-direction: column;
     align-items: center;
     color:#fff;
-   
     &>h1{
         margin-top:15vh;
         font-size: 40px;
@@ -27,8 +48,22 @@ const StyledIndexFon = styled.div`
         margin-top:6vh;
         cursor: pointer;
         font-size: 24px;
-        text-decoration: underline;
         text-shadow: 0px 1px 5px black;
+        transition: 0.3s;
+        position: relative;
+    }
+    &>p::before{
+        position:absolute;
+        bottom:0;
+        left:0;
+        width:100%;
+        border-bottom:3px solid #fff;
+        transition: 0.3s;
+        content:"";
+    }
+    &>p:hover::before{
+     animation: ${AnimationBtnFon} 1s linear ;
+     bottom:100%;
     }
 `
 
@@ -46,7 +81,7 @@ const Index: FC = () => {
                 <p>Забронировать сейчас</p>
             </StyledIndexFon>
             <StyledIndexInfo>
-                
+
             </StyledIndexInfo>
         </div>
     )
